7413513495c30c252138b739380c116515d46ba0
Abdarrahman Ayyaz <35638978+AbdarrahmanAyyaz@users.noreply.github.com>
2022-10-10 20:53:01 -0700

    Update StackInterface.java

--- StackInterface.java
+++ StackInterface.java
@@ -3,18 +3,11 @@
  * San Francisco State University
  * CSC 220 -  Data Structures
  * File Name: StackInterface.java
- * Author: Frank M. Carrano
- * Author: Timothy M. Henry
- * Author: Duc Ta
- * Author: <First Name> <Last Name>
+ * Author: Abdarrahman Ayyaz
  * **********************************************
  */
 
-//
-// Please DO NOT CHANGE this file.
-// Please DO NOT CHANGE this file.
-// Please DO NOT CHANGE this file.
-//
+
 
 package assignment03PartB;
 
